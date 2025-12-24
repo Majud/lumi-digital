ec3edc98450e3abe93da313b468f679153743f12
Majud <91309100+Majud@users.noreply.github.com>
2025-12-24 23:13:12 +0100

    gym

---
+++ demos/gmy/assets/js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+burger?.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
+
+// close menu after click
+mobileMenu?.querySelectorAll("a").forEach(a => {
+  a.addEventListener("click", () => mobileMenu.classList.remove("open"));
+});
